9b47bf8321ca74ffa51bc8979ae5e55ab41d2d72
aveselinovic <aveselinovic.90@gmail.com>
2024-03-04 00:49:08 +0100

    TASK-1 update package name

--- luma-accordia/src/main/java/org/luma/selenium/App.java
+++ luma-accordia/src/main/java/org/luma/selenium/App.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.luma.selenium;
 
 /**
  * Hello world!
--- luma-accordia/src/test/java/org/luma/selenium/AppTest.java
+++ luma-accordia/src/test/java/org/luma/selenium/AppTest.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.luma.selenium;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
