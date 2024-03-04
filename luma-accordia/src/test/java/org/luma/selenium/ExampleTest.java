d0915a39e5bb8a4ba01bc63ba79faeb81754910c
aveselinovic <aveselinovic.90@gmail.com>
2024-03-04 04:44:58 +0100

    TASK-1 add parallel exec support

---
+++ luma-accordia/src/test/java/org/luma/selenium/ExampleTest.java
@@ -0,0 +1,25 @@
+package org.luma.selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class ExampleTest extends BaseTest {
+
+    @Test
+    public void testAnotherPageTitle() {
+        // Open another website
+        driver.get("https://www.example.com");
+
+        // Verify page title
+        String pageTitle = driver.getTitle();
+        System.out.println("Page title: " + pageTitle);
+
+        // Assert that the page title contains "Example"
+        Assert.assertTrue(pageTitle.contains("Example Domain"), "Page title doesn't contain 'Example'");
+    }
+}
